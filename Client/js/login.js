55867111964c0b2c7337541dfe6021d5dde7945d
trungdp <trungdp.labianlabs@gmail.com>
2019-04-11 11:32:18 +0700

    add style

--- Client/js/login.js
+++ Client/js/login.js
@@ -1,16 +1,13 @@
 $(function() {
-    var mongodb = require('./Database/MongoDB');
     var modal = $('#id01');
 
     modal.on('click', function() {
         modal.style.display = "none";
     });
 
-    $('#btn-test').on('click', function() {
-        alert("login");
+    $('#btn-continute').on('click', function() {
         var name = $('#username').val();
         var pass = $('#password').val();
-        alert("login");
         if (mongodb.isValidateUser({ name: name, pass: pass })) {
             alert("true");
         } else {
