b75dbfebc5461e0abed3dd5b820ee1fa84f9770f
anhvupt <anhvupt2412@gmail.com>
2019-04-20 17:52:43 +0700

    20/4

--- Client/js/chat.js
+++ Client/js/chat.js
@@ -1,13 +1,13 @@
-$(function () {
+$(function() {
     //Kết nối tới server socket đang lắng nghe
     var socket = io.connect('http://localhost:3000');
 
     //Socket nhận data và append vào giao diện
-    socket.on("send", function (data) {
+    socket.on("send", function(data) {
         var username = $('#ip-user-name').val();
         var owner = 'owner-message';
         var friend = 'friends-message';
-        if (data.username === username){
+        if (data.username === username) {
             convertMessage(owner, data);
         } else {
             $('#friend-name').text(data.username);
@@ -15,35 +15,82 @@ $(function () {
         }
     });
 
-    $("#btn-send").on('click', function () {
+    $("#btn-send").on('click', function() {
         sendMessage();
     });
-    $("#message-input").on('keypress', (e)=>{
+    $("#message-input").on('keypress', (e) => {
         //vì một số browser dùng keyCode, một số dùng keyWhich, nên lấy 1 trong 2
         var keyCode = (e.keyCode ? e.keyCode : e.keyWhich);
-        if(keyCode == '13'){
+        if (keyCode == '13') {
             sendMessage();
         }
     });
 
-//**************************************************************************
-//Hàm xử lý
-//**************************************************************************
-    var convertMessage = (who,data)=> {
-        $("#messages").append("<li  class="+who+"> <p>" 
-                + data.username + ": " + data.message + " </p>"
-                +"</li>");
+    $('#float-button').on('click', ()=>{
+        $('#signin').show();
+    })
+    $('#btn-skip').on('click', ()=>{
+        $('#signin').hide();
+        $('#float-button').show();
+    });
+
+    var btnSwitch = $('#btn-switch-login');
+    var title = $('#h1-title');
+    var note = $('#bottom-note');
+
+    btnSwitch.on('click', () => {
+        console.log('btn switch');
+        if (btnSwitch.text() === 'Đăng nhập') {
+            showLogin();
+            btnSwitch.text('Đăng ký');
+        } else { 
+            showSignin();
+            btnSwitch.text('Đăng nhập');
+        }
+    });
+
+    $('#btn-continue').on('click', ()=>{
+        if(title.text() === 'ĐĂNG NHẬP'){
+            console.log('login buttton clicked');
+            //logInfunction();
+        }else{
+            console.log('signin buttton clicked');
+            //signInFunction();
+        }
+    })
+
+
+    //**************************************************************************
+    //Hàm xử lý
+    //**************************************************************************
+    var convertMessage = (who, data) => {
+        $("#messages").append("<li  class=" + who + "> <p>" +
+            data.username + ": " + data.message + " </p>" +
+            "</li>");
     }
-    var sendMessage = ()=>{
+    var sendMessage = () => {
         var username = $('#ip-user-name').val();
         var message = $('#message-input').val();
         var sendTime = new Date().getHours();
-        if ( message === '') {
+        if (message === '') {
             alert('Please enter name and message!!');
         } else {
             $('#message-input').val('');
             //Gửi dữ liệu cho socket
-            socket.emit('send', {username: username, message: message, sendTime: sendTime});
+            socket.emit('send', { username: username, message: message, sendTime: sendTime });
         }
+    }
+
+    var showLogin = () => {
+        var signinNote = "<p id='bottom-note'><a id='note-link' href='#''>Quên mật khẩu?</a></p>"
+        title.text('ĐĂNG NHẬP');
+        note.empty();
+        note.append(signinNote);
+    }
+    var showSignin = () => {
+        var signinNote = "<p id='bottom-note'><a id='note-link' href='#''>Đăng nhập</a> nếu bạn đã là thành viên của DLChat</p>"
+        title.text('ĐĂNG KÝ');
+        note.empty();
+        note.append(signinNote);
     }
 });
